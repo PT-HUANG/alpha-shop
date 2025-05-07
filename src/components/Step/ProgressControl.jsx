b8333227ede6eed7758b19c3865ec74ff5d8eb73
PT-HUANG <qwe1593574268@gmail.com>
2025-05-07 13:28:05 +0800

    fix: image path

--- src/components/Step/ProgressControl.jsx
+++ src/components/Step/ProgressControl.jsx
@@ -1,31 +1,32 @@
-export default function ProgressControl({handleClick, handleConsole, currentStep}) {
+export default function ProgressControl({
+  handleClick,
+  handleConsole,
+  currentStep,
+}) {
   return (
     <>
       <section className="progress-control-container col col-lg-6 col-sm-12">
-          <section className="button-group col col-12">
-            {currentStep > 1 && 
-              <button 
-              className="prev"
-              onClick={handleClick}
-              >
-                <object
-                  data="./public/icons/left-arrow.svg"
-                  className="cursor-point"
-                ></object>
-                上一步
-              </button>
-            }
-            <button 
-              className="next"
-              onClick={currentStep === 3 ? handleConsole : handleClick}
-            >
-              {currentStep === 3 ? '確認下單' : '下一步'}
+        <section className="button-group col col-12">
+          {currentStep > 1 && (
+            <button className="prev" onClick={handleClick}>
               <object
-                data="./public/icons/right-arrow.svg"
+                data="icons/left-arrow.svg"
                 className="cursor-point"
               ></object>
+              上一步
             </button>
-          </section>
+          )}
+          <button
+            className="next"
+            onClick={currentStep === 3 ? handleConsole : handleClick}
+          >
+            {currentStep === 3 ? "確認下單" : "下一步"}
+            <object
+              data="icons/right-arrow.svg"
+              className="cursor-point"
+            ></object>
+          </button>
+        </section>
       </section>
     </>
   );
